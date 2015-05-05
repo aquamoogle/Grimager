6a1af1245c2a6959740f4258cff62412dabaad61
Andrew Beers <abeers@loyaltree.com>
2015-05-04 21:26:26 -0400

    The initial and likely only commit to this project unless for some reason someone actually requests an enhancement

---
+++ Grimager/ImageProcessing/Settings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Grimager.ImageProcessing
+{
+    public class Settings
+    {
+        protected Settings()
+        {
+            var cfg = ConfigurationManager.AppSettings;
+            Width = int.Parse(cfg["Image.Width"]);
+            Height = int.Parse(cfg["Image.Height"]);
+            Spacing = int.Parse(cfg["Image.Spacing"]);
+            LetterBoxSize = int.Parse(cfg["Image.Numbering.Size"]);
+        }
+
+        private static Settings _instance;
+
+        public static Settings Get()
+        {
+            if (_instance == null)
+            {
+                _instance = new Settings();
+            }
+
+            return _instance;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Border { get; set; }
+        public int Spacing { get; set; }
+        public int LetterBoxSize { get; set; }
+    }
+}
---
+++ Grimager/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Grimager.ImageProcessing;
+
+namespace Grimager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char rerun = '1';
+
+            do
+            {
+                int columns = 2;
+                var columnInput = String.Empty;
+                do
+                {
+                    Console.WriteLine("How many columns would you like (an integer):");
+                    columnInput = Console.ReadLine();
+                } while (!int.TryParse(columnInput, out columns));
+
+                var processor = new ImageProcessor(columns);
+
+                Console.WriteLine("Specify the directory path for the source images:");
+                var source = Console.ReadLine();
+                Console.WriteLine("Specify the full path of the output file you would like:");
+                var destination = Console.ReadLine();
+
+                var output = processor.ProcessTo(source, destination);
+
+                foreach (var resp in output)
+                {
+                    Console.WriteLine(output);
+                }
+
+                Console.Write("Press 1 to exit or 2 to run again..");
+                rerun = Console.ReadKey().KeyChar;
+                Console.Clear();
+            } while (rerun == '2');
+            //processor.ProcessTo("D:\\Spikes\\Grimager\\Grimager\\Sample\\4302-2E&F", "D:\\Spikes\\Grimager\\Grimager\\Sample\\TestImage.jpeg");
+        }
+    }
+}
